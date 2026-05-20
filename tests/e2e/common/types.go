4dda501f10b48b1b7f38fcb7616b56a5b7aa7b68
afa-farkhod <af4092@ajou.ac.kr>
2026-05-20 16:39:40 +0900

    On branch af/analyze
     Changes to be committed:
    	modified:   tests/e2e/common/types.go
    	modified:   tests/e2e/e2e_setup_test.go
    	new file:   tests/e2e/e2e_test_logger.go
    	new file:   tests/e2e/interchain_test_helper.go

--- tests/e2e/common/types.go
+++ tests/e2e/common/types.go
@@ -1,12 +1,16 @@
 package common
 
 import (
+	"testing"
+	"time"
+
 	"github.com/ory/dockertest/v3"
 	"github.com/stretchr/testify/suite"
 )
 
 type TestingSuite struct {
 	suite.Suite
+	Logger       Logger // Logger for test output
 	TestCounters TestCounters
 	Resources    Resources
 }
@@ -28,3 +32,32 @@ type Resources struct {
 
 	ValResources map[string][]*dockertest.Resource
 }
+
+// ============================================================================
+// LOGGER INTERFACE - ADD THIS SECTION
+// ============================================================================
+
+// Logger is the interface for test logging
+type Logger interface {
+	StartTest(testName string)
+	PassTest(testName string)
+	FailTest(testName string, err error)
+	LogStep(stepName string, details ...interface{})
+	LogInfo(format string, args ...interface{})
+	LogError(format string, args ...interface{})
+	LogSubTest(subTestName string)
+	LogDebug(format string, args ...interface{})
+	LogWarning(format string, args ...interface{})
+	LogSuccess(format string, args ...interface{})
+	LogFailure(format string, args ...interface{})
+	LogSection(sectionName string)
+	LogSeparator()
+	LogWithTime(format string, args ...interface{})
+	SetVerbose(verbose bool)
+	IsVerbose() bool
+	GetCurrentTest() string
+	GetElapsedTime() time.Duration
+}
+
+// NewTestLogger is a variable that will be set by the e2e package
+var NewTestLogger func(t testing.TB, verbose bool) Logger
